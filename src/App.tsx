0251e37944d2034d1c95f8b1aaeefaf79eeb8426
gregoryls1 <contatogregory@gmail.com>
2025-02-09 13:19:23 -0300

    configuraçao tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-xl font-bold text-blue-500">
+        TailwindCSS Configurado! 🚀
+      </h1>
+    </div>
   )
 }
 
